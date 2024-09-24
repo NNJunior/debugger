ec0d3d1b3238dac750e85a4b5ce4a05c83ae3f96
stupidlucy <27612785+NNJunior@users.noreply.github.com>
2024-09-24 14:31:54 +0300

    Changed template files

--- template/testing/scripts/check.sh
+++ template/testing/scripts/check.sh
@@ -1,4 +1,5 @@
 # This is a file to run your 'checker'
 # Checker is a program that takes test and answer as input and returns 1 if the answer is correct, else 0
 
-./path-to-checker
+# ./path-to-checker
+# python3 path/to/checker.py
--- template/testing/scripts/generate.sh
+++ template/testing/scripts/generate.sh
@@ -1,4 +1,5 @@
 # This is a file to run your 'generator'
 # Generator is a program that randomly generates tests and outputs them to stdout
 
-./path-to-generator
+# ./path-to-generator
+# python3 path/to/generator.py
